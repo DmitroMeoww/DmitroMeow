50aca3096c4fc9ff18669e9367583818fa5de32f
DmitroMeoww <meowdmitro@gmail.com>
2025-05-20 12:15:12 +0300

    join done.

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const axios = require("axios");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const { use } = require("react");
 const path = require("path");
 const { connect } = require("http2");
 const pgp = require("pg-promise")();
@@ -38,15 +37,18 @@ const Update_JWT = {
 async function getUserByUsername(username) {
   return new Promise((resolve, reject) => {
     database
-      .one("SELECT * FROM users WHERE username = $1", [username])
+      .oneOrNone("SELECT * FROM users WHERE username = $1", [username])
       .then((user) => resolve(user))
-      .catch((error) => reject(error));
+      .catch((error) => {
+        reject(error);
+        console.log(error);
+      });
   });
 }
 async function getUserById(id) {
   return new Promise((resolve, reject) => {
     database
-      .one("SELECT * FROM users WHERE id = $1", [id])
+      .oneOrNone("SELECT * FROM users WHERE id = $1", [id])
       .then((user) => resolve(user))
       .catch((error) => reject(error));
   });
@@ -60,7 +62,10 @@ async function addUser(username, password) {
         hashedPassword,
       ])
       .then((data) => resolve(data.id))
-      .catch((error) => reject(error));
+      .catch((error) => {
+        reject(error);
+        console.log(error);
+      });
   });
 }
 
@@ -82,19 +87,19 @@ async function createJwt(userId) {
       expiresIn: "3d",
     });
     sessions.set(updatetoken, userId);
-    resolve({ token, updatetoken });
+    resolve({ token: token, updatetoken: updatetoken });
   });
 }
 async function cookieUser(token, updatetoken, res) {
   return new Promise((resolve, reject) => {
-    const userId = sessions.get(token);
-    if (userId) {
+    try {
       res.cookie("access_token", token, Access_JWT);
       res.cookie("update_token", updatetoken, Update_JWT);
-      resolve(userId);
-    } else {
-      reject("Invalid token");
+    } catch (err) {
+      console.log(err);
+      reject("Error creating cookies");
     }
+    resolve();
   });
 }
 async function checkUser(req, res) {
@@ -108,6 +113,7 @@ async function checkUser(req, res) {
         })
         .catch((err) => {
           reject("Invalid or expired token");
+          console.log(err);
         });
     } else if (updatetoken) {
       verifyJwt(updatetoken)
@@ -124,17 +130,21 @@ async function checkUser(req, res) {
                   })
                   .catch((err) => {
                     reject("Invalid or expired token");
+                    console.log(err);
                   });
               })
               .catch((err) => {
+                console.log(err);
                 reject("Invalid or expired token");
               });
           }
         })
         .catch((err) => {
+          console.log(err);
           reject("Update token used/expired/invalid");
         });
     } else {
+      console.log("No tokens provided");
       reject("No tokens provided");
     }
   });
@@ -169,17 +179,17 @@ app.get("/home", getLimiter, (req, res) => {
 app.get("/account", getLimiter, (req, res) => {
   checkUser(req, res)
     .then((userId) => {
-      res.redirect("/account");
+      res.sendFile(path.join(__dirname, "public", "account.html"));
     })
     .catch((rejection) => {
-      res.sendFile(path.join(__dirname, "public", "join.html"));
+      res.redirect("/join");
     });
 });
 
 app.get("/join", getLimiter, (req, res) => {
   checkUser(req, res)
     .then((userId) => {
-      res.redirect("/home");
+      res.redirect("/account");
     })
     .catch((rejection) => {
       res.sendFile(path.join(__dirname, "public", "join.html"));
@@ -213,10 +223,12 @@ app.post("/login", loginLimiter, (req, res) => {
                       res.redirect("/home");
                     })
                     .catch((err) => {
+                      console.log(err);
                       res.status(500).send("Error creating cookies");
                     });
                 })
                 .catch((err) => {
+                  console.log(err);
                   res.status(500).send("Error creating JWT");
                 });
             } else {
@@ -224,6 +236,7 @@ app.post("/login", loginLimiter, (req, res) => {
             }
           })
           .catch((err) => {
+            console.log(err); // log the actual error for debugging
             res.status(500).send("Error comparing passwords");
           });
       } else {
@@ -231,6 +244,7 @@ app.post("/login", loginLimiter, (req, res) => {
       }
     })
     .catch((err) => {
+      console.log(err);
       res.status(500).send("Error getting user");
     });
 });
@@ -259,64 +273,72 @@ app.post("/signup", signupLimiter, (req, res) => {
           .then((userId) => {
             createJwt(userId)
               .then((result) => {
-                const token = result.token;
-                const updatetoken = result.updatetoken;
+                const token = result[0];
+                const updatetoken = result[1];
                 cookieUser(token, updatetoken, res)
                   .then((userId) => {
                     res.redirect("/home");
                   })
                   .catch((err) => {
+                    console.log(err);
                     res.status(500).send("Error creating cookies");
                   });
               })
               .catch((err) => {
+                console.log(err);
                 res.status(500).send("Error creating JWT");
               });
           })
           .catch((err) => {
+            console.log(err);
             res.status(500).send("Error adding user");
           });
       }
     })
     .catch((err) => {
+      console.log(err);
       res.status(500).send("Error getting user");
     });
 });
 
-app.post(
-  "/secretsbyotherservice",
-  ratelimit({
-    windowMs: 60 * 1000, // 1 minute
-    max: 1, // limit each IP to 5 requests per windowMs
-    message: "Too many requests, please try again later",
-  }),
-  (req, res) => {
-    if (SERVER_VERIFICATED) {
-      res.status(403).send("Server already verified");
-      return;
-    }
-    const data = req.body.split(";");
-    jwtSecret = data[0];
-    Imgur_client_id = data[1];
-    Weather_api_key = data[2];
-    const cn = {
-      connectionString: data[3],
-      max: 30,
-    };
-    database = pgp(cn);
-    database
-      .connect()
-      .then((obj) => {
-        obj.done(); // success
-        console.log("DB connected");
-      })
-      .catch((error) => {
-        console.error("DB connection error:", error);
-      });
-    SERVER_VERIFICATED = true;
-    res.status(200).send("Data received");
-  }
-);
+// app.post(
+//   "/secretsbyotherservice",
+//   ratelimit({
+//     windowMs: 60 * 1000, // 1 minute
+//     max: 1, // limit each IP to 5 requests per windowMs
+//     message: "Too many requests, please try again later",
+//   }),
+//   (req, res) => {
+//     if (SERVER_VERIFICATED) {
+//       res.status(403).send("Server already verified");
+//       return;
+//     }
+// const data = req.body.split(";");
+const data =
+  "1c0fb5008c573315e7b1e1af5ab41d0ce9b8d4469e41c4d59c3041bd99671208c415fcb0359418dd6bc481863d3d5d030a75364318afbec54cdba082df3f9577;519c7eae1a4a75b;5f6f29da20324c2499e192710251004;postgresql://neondb_owner:npg_YTIMu8Ek2Ucm@ep-summer-sound-a2xoc786-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require".split(
+    ";"
+  );
+JwtSecret = data[0];
+Imgur_client_id = data[1];
+Weather_api_key = data[2];
+const cn = {
+  connectionString: data[3],
+  max: 30,
+};
+database = pgp(cn);
+database
+  .connect()
+  .then((obj) => {
+    obj.done(); // success
+    console.log("DB connected");
+  })
+  .catch((error) => {
+    console.error("DB connection error:", error);
+  });
+SERVER_VERIFICATED = true;
+// res.status(200).send("Data received");
+//   }
+// );
 
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
@@ -324,5 +346,5 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
   console.log("Server started successfuly!");
-  axios.post("https://hook.eu2.make.com/qjlhykh1cfkr8ygtr7cikfjbqq6jd28p");
+  // axios.post("https://hook.eu2.make.com/qjlhykh1cfkr8ygtr7cikfjbqq6jd28p");
 });
